877d346e97e1d343a773356fb7afe69008f2dd9a
Christine Santos <56892403+christinemsantos@users.noreply.github.com>
2024-02-28 14:34:44 -0500

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import "./index.css";
 import WeatherSearch from "./WeatherSearch";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather Search</h1>
       <WeatherSearch />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      Coded by Christine Santos and Open-sourced on
+      <a href="https://github.com/christinemsantos"> GitHub.</a>
+    </footer>
+  );
+}
